6eb73d1e944260972450b222afde3531084cf798
pr0x1es <dp98528@outlook.com>
2024-07-07 16:24:11 -0500

    fix

--- components/Shorts.tsx
+++ components/Shorts.tsx
@@ -1,37 +1,77 @@
 import Image from "next/image";
 import Link from "next/link";
+
 export default function Shorts() {
   return (
-    <div className="flex flex-row justify-evenly">
+    <div className="flex justify-evenly flex-col">
       <div className="mt-7">
-        <div className="border-4 rounded-2xl p-5">
-          <h1> My first ASMR video #asmr </h1>
-          <Link href="https://www.youtube.com/watch?v=Th0oRxtqON0">
-            <Image src="/images/3.jpg" alt="" width="400" height="400" />
-          </Link>
-        </div>
-
-        <div className="mt-7">
-          <div className="border-4 rounded-2xl p-5">
-            <h1> English or Spanish #asmr </h1>
-            <Link href="https://www.youtube.com/shorts/p5glJmiSAYo">
-              <Image src="/images/5.jpg" alt="" width="400" height="400" />
+        <div className="border-4 rounded-2xl p-8 bg-stone-950 hover:bg-neutral-900 hover:transition-all transition-all hover:rounded-3xl">
+          <h1 className="text-center mb-7 text-lg">
+            My first ASMR video #asmr
+          </h1>
+          <div className="flex place-content-center">
+            <Link href="https://www.youtube.com/watch?v=Th0oRxtqON0">
+              <Image
+                src="/images/3.jpg"
+                alt=""
+                width="800"
+                height="400"
+                className="rounded-3xl"
+              />
             </Link>
           </div>
-          <div className="mt-7">
-            <div className="border-4 rounded-2xl p-5">
-              <h1> ASMR Brainrot Quiz #asmr #viral #fyp </h1>
-              <Link href="https://www.youtube.com/shorts/C_g1Q7i7Qjk">
-                <Image src="/images/4.jpg" alt="" width="400" height="400" />
+        </div>
+      </div>
+
+      <div className="mt-7">
+        <div className="border-4 rounded-2xl p-8 bg-stone-950 hover:bg-neutral-900 hover:transition-all transition-all hover:rounded-3xl">
+          <h1 className="text-center mb-7 text-lg">English or Spanish #asmr</h1>
+          <div className="flex place-content-center">
+            <Link href="https://www.youtube.com/watch?v=Th0oRxtqON0">
+              <Image
+                src="/images/5.jpg"
+                alt=""
+                width="800"
+                height="400"
+                className="rounded-3xl"
+              />
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-7">
+        <div className="border-4 rounded-2xl p-8 bg-stone-950 hover:bg-neutral-900 hover:transition-all transition-all  hover:rounded-3xl">
+          <h1 className="text-center mb-7 text-lg">
+            ASMR Brainrot Quiz #asmr #viral #fyp
+          </h1>
+          <div className="flex place-content-center">
+            <Link href="https://www.youtube.com/watch?v=Th0oRxtqON0">
+              <Image
+                src="/images/4.jpg"
+                alt=""
+                width="800"
+                height="400"
+                className="rounded-3xl"
+              />
+            </Link>
+          </div>
+        </div>
+        <div className="mt-7">
+          <div className="border-4 rounded-2xl p-8 bg-stone-950 hover:bg-neutral-900 hover:transition-all transition-all  hover:rounded-3xl">
+            <h1 className="text-center mb-7 text-lg">
+              Your Rizz is low #asmr #fyp #viral
+            </h1>
+            <div className="flex place-content-center">
+              <Link href="https://www.youtube.com/watch?v=Th0oRxtqON0">
+                <Image
+                  src="/images/6.jpg"
+                  alt=""
+                  width="800"
+                  height="400"
+                  className="rounded-3xl"
+                />
               </Link>
-            </div>
-            <div className="mt-7">
-              <div className="border-4 rounded-2xl p-5">
-                <h1> Your Rizz is low #asmr #fyp #viral </h1>
-                <Link href="https://www.youtube.com/shorts/390TRZ-v2mA">
-                  <Image src="/images/6.jpg" alt="" width="400" height="400" />
-                </Link>
-              </div>
             </div>
           </div>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,47 +7,51 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <div className="border-2 rounded-md p-7 mt-7">
+      <div className="border-2 rounded-lg p-7 mt-7">
         <h1 className="font-bold text-center text-3xl mb-7">
-          Dalton&apos;s ASMR
+          Dalton&apos;s ASMR 😎
         </h1>
         <div className="mt-7"></div>
         <Link
           href="https://www.youtube.com/@daltonsasmr"
           className="hover:italic hover:transition-all hover:font-bold transition-all"
         >
-          <div className="text-center">Channel link</div>
+          <div className="text-center Home">Channel link</div>
         </Link>
       </div>
 
       <h1 className="text-center font-bold text-2xl mt-7">Videos</h1>
-      <div className="flex flex-row justify-evenly">
+      <div className="flex flex-col justify-evenly">
         <div className="mt-7">
-          <div className="border-4 rounded-2xl p-5">
-            <h1>My first ASMR video 🥲</h1>
+          <div className="border-4 rounded-2xl p-8 bg-stone-950 hover:bg-neutral-900 hover:transition-all transition-all">
+            <h1 className="text-center mb-7 text-lg">My first ASMR video 🥲</h1>
             <Link href="https://www.youtube.com/watch?v=Th0oRxtqON0">
-              <Image src="/images/1.jpg" alt="" width="400" height="400" />
+              <div className="flex place-content-center">
+              <Image src="/images/1.jpg" alt="" width="800" height="400" className="rounded-md" />
+              </div>
             </Link>
           </div>
         </div>
 
         <div className="mt-7">
-          <div className="border-4 rounded-2xl p-5">
-            <h1>[ASMR] Tapping triggers and stuff</h1>
+          <div className="border-4 rounded-2xl p-8 bg-stone-950 hover:bg-neutral-900 hover:transition-all transition-all">
+            <h1 className="mb-7 text-center">[ASMR] Tapping triggers and stuff</h1>
             <Link href="https://www.youtube.com/watch?v=pU0uaOa0d_k">
-              <Image src="/images/2.jpg" alt="" width="400" height="400" />
+              <div className="flex place-content-center">
+              <Image src="/images/2.jpg" alt="" width="800" height="400" className="rounded-md" />
+              </div>
             </Link>
           </div>
         </div>
       </div>
 
-      <h1 className="font-bold text-2xl text-center">Shorts</h1>
+      <h1 className="font-bold text-2xl text-center mt-7">Shorts</h1>
       <div>
         <Shorts />
       </div>
 
       <footer>
-        <div className="border-2 p-6 mt-7">
+        <div className="border-2 p-6 mt-7 rounded-md">
           <h1 className="font-bold">Credits</h1>
           <h1 className="italic">Favicon info:</h1>
           <Link
